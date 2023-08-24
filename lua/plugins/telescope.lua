43b99a52305d389c3f410cc464a4c8b6e59a3ff6
liruijie <liruijie@vyagoo.com>
2023-08-24 16:51:25 +0800

    telescope添加查询历史快捷键，扩展依赖，修改查找文件预览

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,63 @@
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
 
   config = function()
     local actions = require("telescope.actions")
+    -- don't preview binary file
+    local previewers = require("telescope.previewers")
+    local Job = require("plenary.job")
+    local new_maker = function(filepath, bufnr, opts)
+    filepath = vim.fn.expand(filepath)
+    Job:new({
+        command = "file",
+        args = { "--mime-type", "-b", filepath },
+        on_exit = function(j)
+        local mime_type = vim.split(j:result()[1], "/")[1]
+        if mime_type == "text" then
+            previewers.buffer_previewer_maker(filepath, bufnr, opts)
+        else
+            -- maybe we want to write something to the buffer here
+            vim.schedule(function()
+            vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+            end)
+        end
+        end
+    }):sync()
+    end
+
     require("telescope").setup({
       defaults = {
+        buffer_previewer_maker = new_maker, -- don't preview binary file
         mappings = {
           i = {
             ["<C-j>"] = actions.move_selection_next,
             ["<C-k>"] = actions.move_selection_previous,
+            ["<C-p>"] = actions.cycle_history_prev,
+            ["<C-n>"] = actions.cycle_history_next,
           },
         },
       },
+      pickers = {
+        find_files = {
+        theme = "dropdown",
+        previewer = false,
+        find_command = { "fd", "-H" , "-I"},  -- "-H" search hidden files, "-I" do not respect to gitignore
+        -- find_command = { "find", "-type", "f" },
+        },
+        git_files = {
+        theme = "dropdown",
+        previewer = false,
+        find_command = { "fd", "-H" , "-I"},  -- "-H" search hidden files, "-I" do not respect to gitignore
+        -- find_command = { "find", "-type", "f" },
+        },
+      }
     })
   end,
 
